8a8742a6d79d3c64c6e9ecc2bd402119abe9da0a
VicoH <58180744+tralalavico@users.noreply.github.com>
2022-11-16 15:36:47 +0100

    Class Comments.php suite

--- app/src/Entity/Comments.php
+++ app/src/Entity/Comments.php
@@ -2,9 +2,23 @@
 
 class Comments{
 
+    private int $id;
     private string $content;
+    private int $userId;
+    private int $postId;
+    private int $comId;
     private string $dateTime;
 
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): int
+    {
+        $this->id = $id;
+        return $this;
+    }
 
     public function getContent(): string
     {
@@ -14,6 +28,39 @@ class Comments{
     public function setContent(string $content): string
     {
         $this->content = $content;
+        return $this;
+    }
+
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): int
+    {
+        $this->userId = $userId;
+        return $this;
+    }
+
+    public function getPostId(): int
+    {
+        return $this->postId;
+    }
+
+    public function setPostId(int $postId): int
+    {
+        $this->postId = $postId;
+        return $this;
+    }
+
+    public function getComId(): int
+    {
+        return $this->comId;
+    }
+
+    public function setComId(int $comId): int
+    {
+        $this->comId = $comId;
         return $this;
     }
 
