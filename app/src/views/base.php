8943748a2f05deca8215f96ac0b7c732e6b2f479
VicoH <58180744+tralalavico@users.noreply.github.com>
2022-11-20 00:36:36 +0100

    Tailwind css

--- app/src/views/base.php
+++ app/src/views/base.php
@@ -5,10 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title><?= $_pageTitle; ?></title>
 </head>
-<body>
-
-    <?= $_pageContent; ?>
+<body class="bg-slate-200 flex justify-center">
+    <div class="grid gap-5 w-full h-full">
+        <?= $_pageContent; ?>
+    </div>
+    
 </body>
 </html>
--- app/src/views/homepage.php
+++ app/src/views/homepage.php
@@ -3,10 +3,13 @@
 
 <?php
     foreach ($Post as $post) { ?>
-    <h2><?php echo $post->getTitle();?></h2>
-    <img src="<?php echo $post->getImage();?>" alt="Image de Chat">
-    <p><?php echo $post->getContent();?></p>
-    <p><?php echo $post->getDateTIme();?></p>
+    <div class="sm:w-96 grid gap-5 container rounded-md bg-white p-10">
+        <h2 class="text-3xl text-neutral-900 font-semibold"><?php echo $post->getTitle();?></h2>
+        <div class="h-96 w-full rounded-md bg-[url('<?php echo $post->getImage();?>')] bg-cover bg-center"></div>
+        <p><?php echo $post->getContent();?></p>
+        <p><?php echo $post->getDateTIme();?></p>
+    </div>
+    
 <?php 
 }
 
