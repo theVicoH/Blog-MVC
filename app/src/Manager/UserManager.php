d4648c19d1a211aa38e41248f0ca688ddd8aeb25
VicoH <58180744+tralalavico@users.noreply.github.com>
2022-11-20 04:42:39 +0100

    SUite changement role

--- app/src/Manager/UserManager.php
+++ app/src/Manager/UserManager.php
@@ -71,7 +71,7 @@ class UserManager extends BaseManager
 
     public function insertUser(User $user): bool
     {
-        $query = $this->pdo->prepare("INSERT INTO User (username, password, email) VALUES (:username, :password, :email");
+        $query = $this->pdo->prepare("INSERT INTO User (username, password, email) VALUES (:username, :password, :email);");
         $query->bindValue('username', $user->getUsername());
         $query->bindValue('password', $user->getPassword());
         $query->bindValue('email', $user->getEmail());
